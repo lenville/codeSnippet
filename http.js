0d19b3fa92eb35cc2417a30f47e3f57301a53793
lenville <lenville@gmail.com>
2013-10-13 20:32:37 +0800

    Update http.js to 0.0.2

--- http.js
+++ http.js
@@ -1,10 +1,12 @@
 //! http.js
-//! Version : 0.0.1
+//! Version : 0.0.2
 //! Authors : Lenville
 //! Site: lenville.com
-//! New Features: 仿照moment.js写http.js的post传递信息+回传 & jsonp实现
+//! New Features: 简化代码，进行高层次封装
+//! History: 
+//!		0.0.1 仿照moment.js写http.js的post传递信息+回传 & jsonp实现
 
-(function (undefined) {
+define(function (require, exports) {
 
 	/************************************
 		常量 // Constants
@@ -24,40 +26,60 @@
 	document.domain = "qq.com";
 
 	/************************************
-		Constructors
-	************************************/
-
-	//HTTP Prototype Object
-	function HTTP(config) {
-		extend(this, config);
-	}
-
-	/************************************
 		Top Level Functions
 	************************************/
-
-	http.prototype.getJson = function (url, callback) {
-		var script = document.createElement("script");
+	
+	exports.getJson = function (url, fn, timeout) {
+		var timeout = timeout || _TIMEOUT,					//超时初始化
+			rand = (Math.random().toString().substr(2, 5)),	//取随机数
+			callback = 'callback_' + rand,					//取回调名称
+			timer,											//其它杂项初始化
+			script = document.createElement("script");
 		script.setAttribute("src", url+'?callback='+callback);
 		document.head.appendChild(script);
+
+		window[callback] = function(data){
+			clearTimeout(timer);
+			fn(data);
+			window[callback] = null;
+		}
+		//超时
+		timer = setTimeout(function(){
+			window[callback]({
+				code: OVERTIME_CODE,
+				msg: OVERTIME_MSG
+			});
+		},timeout);
 	};
 
-	http.prototype.post = function (url, data, fn, timeout) {
+	exports.post = function (url, data, fn, timeout) {
+		
+		var timeout = timeout || _TIMEOUT,					//超时初始化
+			form, iframe, input = [],						//
+			rand = (Math.random().toString().substr(2, 5)),	//取随机数
+			callback = 'callback_' + rand,					//取回调名称
+			timer, counter;									//其它杂项初始化
 
-		var timeout = timeout || _TIMEOUT,
-			timeStamp = new Date().getTime(),
-			callback = 'callback_' + timeStamp,
-			timer,
-			iframe,
-			form,
-			hiddenField;
+		/**/
+		var elementCreate = function(element, parent, params){
+			var temp_element = document.createElement(element);
+			for(var key in params){
+				if(params.hasOwnProperty(key)){
+					temp_element.setAttribute(key, params[key]);
+				}
+			}
+			parent.appendChild(temp_element);
+			key=null;
+			return temp_element;
+		}
 
 		window[callback] = function(data){
 			clearTimeout(timer);
 			fn(data);
 
-			//清理
-			var gcList = [timeStamp, 'sender'];//存放预清理节点ID
+			// 清理
+/*待更改：这样写是否OK，是否保留删除节点的功能*/
+			var gcList = ["frame" + "_" + rand, "form" + "_" + rand];//存放预清理节点ID
 			for (var i in gcList) {
 				if(gcList[i]){
 					var x = document.getElementById(gcList[i]);
@@ -67,37 +89,34 @@
 			window[callback] = null;
 		};
 		
-		iframe = document.createElement("iframe");
-			iframe.setAttribute("id", timeStamp);
-			iframe.setAttribute("name", timeStamp);
-			iframe.setAttribute("style", "display: none;");
-
-		form = document.createElement("form");
-			form.setAttribute("id", "sender");
-			form.setAttribute("method", "post");
-			form.setAttribute("action", url);
-			form.setAttribute("target", timeStamp);
-
-		hiddenField = document.createElement("input");
-			hiddenField.setAttribute("type", "hidden");
-			hiddenField.setAttribute("name", "_callback");
-			hiddenField.setAttribute("value", callback);
-
-			form.appendChild(hiddenField);
-
-		for(var key in data){
-			if(data.hasOwnProperty(key)){
-				hiddenField = document.createElement("input");
-					hiddenField.setAttribute("type", "hidden");
-					hiddenField.setAttribute("name", key);
-					hiddenField.setAttribute("value", data[key]);
-
-				form.appendChild(hiddenField);
+		iframe = elementCreate("iframe", document.body, {
+				"id": "frame" + "_" + rand,
+				"name": "frame" + "_" + rand,
+				"style": "display: none"
 			}
+		);
+
+		form = elementCreate("form", document.body, {
+				"id": "form" + "_" + rand,
+				"name": "form" + "_" + rand,
+				"method": "post",
+				"action": url,
+				"target": "frame" + "_" + rand
+			}
+		);
+
+		if(!("_callback" in data)){
+			data["_callback"] = callback;
 		}
-		
-		document.body.appendChild(form);
-		document.body.appendChild(iframe);
+
+		for(counter in data){
+			var temp_obj = {};
+			temp_obj.type = "hidden";
+			temp_obj.name = counter;
+			temp_obj.value = data[counter];
+			elementCreate("input", form, temp_obj);
+			temp_obj = null;
+		}
 
 		form.submit();
 
@@ -110,38 +129,4 @@
 		},timeout);
 
 	};
-
-	/************************************
-		http Prototype
-	************************************/
-
-	extend(http.fn = Http.protype, {
-		send: function(opt, callback) {
-			switch(true){
-				case opt['method'] == "POST" || opt['method'] == "post":
-					postData(opt['url'], opt['data'], callback);
-				case opt['method'] == "GET" || opt['method'] == "get";
-					getData(opt['url'], opt['data'], callback);
-			}
-		}
-	})
-
-
-	// CommonJS module is defined
-	if (hasModule) {
-		module.exports = http;
-	}
-	/*global ender:false */
-	if (typeof ender === 'undefined') {
-		// here, `this` means `window` in the browser, or `global` on the server
-		// add `http` as a global object via a string identifier,
-		// for Closure Compiler "advanced" mode
-		this['http'] = http;
-	}
-	/*global define:false */
-	if (typeof define === "function" && define.amd) {
-		define("http", [], function () {
-			return http;
-		});
-	}
-}).call(this);
+});

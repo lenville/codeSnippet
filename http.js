9b129b7545ec7704b247019e3791f99768019500
lenville <lenville@gmail.com>
2013-10-13 20:33:41 +0800

    Update http.js to 0.1.0
    将get与post方法合并为ajax
    更正非JSON的get数据处理Bug
    精简代码，优化代码实现

--- http.js
+++ http.js
@@ -1,179 +1,162 @@
-//! http.js
-//! Version : 0.0.3
-//! Authors : Lenville
-//! Site: lenville.com
-//!	New Features: 修改0.0.2版本的post方法的控制方式改为传入options
-//!			  	  修改0.0.2版本的post方法的控制方式改为传入options
-//!			  	  优化代码实现
-//!	History:
-//!		0.0.2 简化代码，重新进行封装
-//!		0.0.1 仿照moment.js写http.js的post传递信息+回传 & jsonp实现
+/**http.js
+ * Version : 0.1.0
+ * Authors : Lenville
+ * Site: lenville.com 
+ * New Features: 将get与post方法合并为ajax
+ *               更正非JSON的get数据处理Bug
+ *               精简代码，优化代码实现
+ * History:
+ *      0.0.3 修改0.0.2版本的post方法的控制方式改为传入options
+ *            修改0.0.2版本的post方法的控制方式改为传入options
+ *            优化代码实现
+ *		0.0.2 简化代码，重新进行封装
+ *		0.0.1 仿照moment.js写http.js的post传递信息+回传 & jsonp实现
+ */
 
-define(function (require, exports) {
+define (function (require, exports) {
 
-	/************************************
-			常量 // Constants
-	************************************/
-
-	var OVERTIME_CODE = 404,
-		OVERTIME_MSG = "超时",
-		_TIMEOUT = 5000;
-
-	/************************************
-			Top Level Functions
-	 ************************************	
-		var options = {
-			url: ,			// 必需
-			charset: ,
-			timeout: ,
-			data: ,			// post模式必需
-			context: 
-		}
-	************************************/
-
-	exports.get = function (options, callback) {
-		options = options || {};
-		var rand = (Math.random().toString().substr(2,5)),
-			cbName = options.cbName || "callback_" + rand,	// callback name
-			timeout = options.timeout || _TIMEOUT,
-			node = document.createElement("script"),
-			urlPat = /callback=\?/,
-			getMode = urlPat.test(options.url),	// 有callback为True，否则为False
-			head = document.head || document.getElementsByTagName("head")[0] || 
-				document.documentElement,
-			temObj = {},
-			timer, i;
-
-		node.src = getMode ? options.url.replace("callback=\?", "callback=" + 
-			cbName) : options.url;
-		// callback pattern array
-		node.cbPatArr = getMode ? "" : /q\=\s?/.exec(node.src);
-		// callback pattern
-		node.cbPat = getMode ? "" : node.src.substr(node.cbPatArr.index+2).split(",");
-		node.async = true;
-		node.id = cbName;
-		node.type = "text/javascript";
-		node.charset = options.charset || "utf-8";
-		node.onload = node.onerror = node.onreadystatechange = function() {
-			if (getMode) {
-				timer = setTimeout(function(){
-					head.removeChild(node);
-					window[cbName]({
-						code: OVERTIME_CODE,
-						msg: OVERTIME_MSG
-					});
-					window[cbName] = null;
-				},timeout);
-
-				window[cbName] = function(data){
-					clearTimeout(timer);
-					callback(data);
-					head.removeChild(node);
-					window[cbName] = null;
-				};
-			} else {
-
-				timer = setTimeout(function(){
-					head.removeChild(node);
-					callback({
-						code: OVERTIME_CODE,
-						msg: OVERTIME_MSG
-					});
-					window[cbName] = null;
-				}, timeout);
-
-				window[cbName] = function(){
-					clearTimeout(timer);
-					temObj = {};
-					for(i in node.cbPat){
-						temObj["v_"+node.cbPat[i]] = window["v_"+node.cbPat[i]];
-					}
-					callback(temObj);
-					window[cbName] = null;
-				};
-				window[cbName]();	
-			}
-				
-		};
-
-		head.appendChild(node);
-	};
-
-	exports.post = function (options, callback) {
-		options = options || {};
+	try{
 		document.domain = "qq.com";
-		var timeout = options.timeout || _TIMEOUT,
-			rand = (Math.random().toString().substr(2, 5)),
-			cbName = "callback_" + rand,					// callback name
-			temp_obj = {},
-			form, iframe, timer, counter, temp_element, gcList, i, x;
+	}catch(ex){
+		// console.log(ex);
+	}
 
-		var elementCreate = function(element, parent, params){
-			temp_element = document.createElement(element);
-			for(counter in params){
-				if(params.hasOwnProperty(counter)){
-					temp_element.setAttribute(counter, params[counter]);
+	var	
+		/*↓常量*/
+		_count = 0,
+		_TIMEOUT = 10000,
+		_OVERTIME_CODE = 404,
+		_OVERTIME_MSG = "超时",
+		getMode, cbName, timer,
+		node, tempnode,
+		iframe, form, cfg = {},
+
+		/*↓Regular Expression*/
+		_RE_URL = /callback=\?/,
+		_RE_READY_STATE = /loaded|complete|undefined/,
+
+		/*↓节点操作*/
+		_doc = document,
+		_head = _doc.head ||
+				_doc.getElementsByTagName("head")[0] ||
+				_doc.documentElement,
+		_body = _doc.body ||
+				_doc.getElementsByTagName("body")[0] ||
+				_doc.documentElement,
+
+		_elementCreate = function (element, parent, params) {
+			tempnode = _doc.createElement(element);
+			tempnode.id = [element, _count].join("_");
+			tempnode.name = [element, _count].join("_");
+			for(var i in params){
+				if(params.hasOwnProperty(i)){
+					tempnode.setAttribute(i, params[i]);
 				}
 			}
-			parent.appendChild(temp_element);
-			counter=null;
-			return temp_element;
-		}
+			parent.appendChild(tempnode);
+			return tempnode;
+		},
+		_defaultCb = function (data) {
+			console.log(data);
+		},
 
-		window[cbName] = function(data){
-			clearTimeout(timer);
-			callback(data);
-
-			// 清理
-/*待更改：这样写是否OK，是否保留删除节点的功能*/
-			gcList = ["frame" + "_" + rand, "form" + "_" + rand];//存放预清理节点ID
-			for (i in gcList) {
-				if(gcList[i]){
-					x = document.getElementById(gcList[i]);
-					x.parentNode.removeChild(x);
-				}
-			}
-			window[cbName] = null;
+		/*↓基本设置*/
+		_config = {
+			method: "GET",
+			charset: "utf-8",
+			timeout: _TIMEOUT,
+			data: {stocklist: "ok"},
+			callback: _defaultCb,
+			context: {}
 		};
-		
-		iframe = elementCreate("iframe", document.body, {
-				"id": "frame" + "_" + rand,
-				"name": "frame" + "_" + rand,
+
+	/*Dora.ajax(url, [settings])*/
+	exports.ajax = function (url, config) {
+		$.extend(cfg, _config, config);
+		cbName = [cfg.method, _count++].join("_");
+
+		if (cfg.method === "POST") {
+
+			iframe = _elementCreate("iframe", _body, {
 				"style": "display: none"
-			}
-		);
-
-		form = elementCreate("form", document.body, {
-				"id": "form" + "_" + rand,
-				"name": "form" + "_" + rand,
-				"method": "post",
-				"action": options.url,
-				"target": "frame" + "_" + rand
-			}
-		);
-
-		if(!("_callback" in options.data)){
-			options.data["_callback"] = cbName;
-		}
-
-		for(counter in options.data){
-			temp_obj = {};
-			temp_obj.type = "hidden";
-			temp_obj.name = counter;
-			temp_obj.value = options.data[counter];
-			elementCreate("input", form, temp_obj);
-			temp_obj = null;
-		}
-
-		form.submit();
-
-		//超时
-		timer = setTimeout(function(){
-			window[cbName]({
-				code: OVERTIME_CODE,
-				msg: OVERTIME_MSG
 			});
-		},timeout);
+			form = _elementCreate("form", _body, {
+				"method": "post",
+				"action": url,
+				"target": ["iframe", _count].join("_")
+			});
 
+			if(!("_callback" in cfg.data)){
+				cfg.data._callback = cbName;
+			}
+
+			for(var i in cfg.data){
+				if(cfg.data.hasOwnProperty(i)){
+					tempnode = {};
+					tempnode.type = "hidden";
+					tempnode.name = i;
+					tempnode.value = cfg.data[i];
+					_elementCreate("input", form, tempnode);
+					tempnode = null;
+				}
+			}
+			
+			window[cbName] = function(data){
+				clearTimeout(timer);
+				_body.removeChild(iframe);
+				_body.removeChild(form);
+				iframe = undefined;
+				form = undefined;
+				cfg.callback(data);
+			};
+
+			form.submit();
+
+			timer = setTimeout(function(){
+				_body.removeChild(iframe);
+				_body.removeChild(form);
+				iframe = undefined;
+				form = undefined;
+				window[cbName]({
+					code: _OVERTIME_CODE,
+					msg: _OVERTIME_MSG
+				});
+			}, cfg.timeout);
+
+			
+		} else if (cfg.method === "GET") {
+			getMode = _RE_URL.test(url),	// 有callback为True，否则为False
+
+			node = _elementCreate("script", _head, {
+				"src": getMode ? url.replace("callback=?", "callback=" + cbName) : url,
+				"async": true,
+				"id": cbName,
+				"type": "text/javascript",
+				"charset": cfg.charset
+			});
+
+			node.onload = node.onerror = node.onreadystatechange = function() {
+				if (_RE_READY_STATE.test(node.readyState)) {
+					window[cbName] = function(){
+						clearTimeout(timer);
+						node.onload = node.onerror = node.onreadystatechange = null;
+						_head.removeChild(node);
+						node = undefined;
+						cfg.callback(window[cfg.context]);
+					};
+					window[cbName]();
+				}
+			};
+
+			timer = setTimeout(function(){
+				node.onload = node.onerror = node.onreadystatechange = null;
+				_head.removeChild(node);
+				cfg.callback({
+					code: _OVERTIME_CODE,
+					msg: _OVERTIME_MSG
+				});
+				window[cbName] = null;
+			}, cfg.timeout);
+		}		
 	};
 });
